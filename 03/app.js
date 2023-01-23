4d4ac7bcdeecdba1028be20644d87cb6b30c6cfb
Katarzyna Zadroga <krzadroga@gmail.com>
2023-01-23 14:39:03 +0100

    Done

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while(randomNumber !== x) {
+    randomNumber = Math.round(Math.random() * 20 );
+    iteration = iteration + 1;
+    console.log(randomNumber, iteration);
+}
+
+console.log('Tyle iteracji potrzeba do wylosowania', iteration);
